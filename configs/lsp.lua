b1511c45264802c1cfe7b1324f1f1cab35b36ab3
Toby Bridle <foreignspaghettibolognese@gmail.com>
2022-08-08 17:11:33 +0100

    Switched barbar for feline.nvim

--- configs/lsp.lua
+++ configs/lsp.lua
@@ -16,6 +16,7 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
 local servers = {
     "sumneko_lua",
     "rust_analyzer",
+    "taplo",
     "clangd",
     "cmake",
     "emmet_ls",
--- plugins.lua
+++ plugins.lua
@@ -1,288 +1,314 @@
 return require("packer").startup(function(use)
-    -- Packer can manage itself
-    use "wbthomason/packer.nvim"
+  -- Packer can manage itself
+  use "wbthomason/packer.nvim"
 
-    -- -- THEMES --
-    -- Gruvbox theme with integration with Treesitter
+  -- -- THEMES --
+  -- Gruvbox theme with integration with Treesitter
 
-    -- use {
-    -- 	"luisiacc/gruvbox-baby",
-    -- 	config = "vim.cmd [[colorscheme gruvbox-baby]]"
-    -- }
+  -- use {
+  --   "luisiacc/gruvbox-baby",
+  --   config = "vim.cmd [[colorscheme gruvbox-baby]]",
+  -- }
 
-    use {
-        "catppuccin/nvim",
-        as = "catppuccin",
-        config = function()
-            vim.g.catppuccin_flavour = "mocha"
-            require("catppuccin").setup { transparent_background = true }
-            vim.cmd [[colorscheme catppuccin]]
-        end
-    }
+  -- use {
+  --   "catppuccin/nvim",
+  --   as = "catppuccin",
+  --   config = function()
+  --     vim.g.catppuccin_flavour = "mocha"
+  --     require("catppuccin").setup { transparent_background = true }
+  --     vim.cmd [[colorscheme catppuccin]]
+  --   end,
+  -- }
+  --
+  use {
+    "folke/tokyonight.nvim",
+    config = function()
+      vim.g.tokyonight_style = "night"
+      vim.g.tokyonight_transparent = true
+      vim.cmd [[colorscheme tokyonight]]
+    end,
+  }
+  use {
+    "nvim-telescope/telescope.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require "configs.telescope"
+    end,
+  }
 
-    use {
-        "nvim-telescope/telescope.nvim",
-        requires = "nvim-lua/plenary.nvim",
-        config = function()
-            require "configs.telescope"
-        end,
-    }
+  use {
+    "stevearc/dressing.nvim",
+    after = "telescope.nvim",
+  }
 
-    use {
-        "stevearc/dressing.nvim",
-        after = "telescope.nvim"
-    }
+  use "kyazdani42/nvim-web-devicons"
 
-    use "kyazdani42/nvim-web-devicons"
+  use {
+    "ojroques/nvim-hardline",
+    config = function()
+      require("hardline").setup {}
+    end,
+  }
 
-    use {
-        "ojroques/nvim-hardline",
-        config = function()
-            require "hardline".setup {}
-        end,
-    }
+  -- LSP: & Completion Stuff
+  use {
+    "neovim/nvim-lspconfig",
+  }
 
-    -- LSP: & Completion Stuff
-    use {
-        "neovim/nvim-lspconfig"
-    }
+  -- use({
+  --   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  --   config = function()
+  --     require("lsp_lines").setup()
+  --   end,
+  -- })
 
-    -- use({
-    --   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-    --   config = function()
-    --     require("lsp_lines").setup()
-    --   end,
-    -- })
+  use {
+    "hrsh7th/nvim-cmp",
+    requires = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-nvim-lua" },
+    config = function()
+      require "configs.cmp"
+    end,
+  }
 
-    use {
-        "hrsh7th/nvim-cmp",
-        requires = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-nvim-lua" },
-    }
+  use { "onsails/lspkind.nvim", requires = "nvim-cmp" }
 
-    use { "onsails/lspkind.nvim", requires = "nvim-cmp" }
+  use {
+    "L3MON4D3/LuaSnip",
+    requires = "nvim-cmp",
+    config = function()
+      require "configs/luasnip"
+    end,
+  }
 
-    use {
-        "L3MON4D3/LuaSnip",
-        requires = "nvim-cmp",
-        config = function()
-            require "configs/luasnip"
-        end,
-    }
+  use {
+    "saadparwaiz1/cmp_luasnip",
+    requires = "LuaSnip",
+  }
 
-    use {
-        "saadparwaiz1/cmp_luasnip",
-        requires = "LuaSnip"
-    }
+  use {
+    "rafamadriz/friendly-snippets",
+  }
 
-    use {
-        "rafamadriz/friendly-snippets",
-    }
+  use {
+    "zbirenbaum/copilot.lua",
+    event = { "VimEnter" },
+    config = function()
+      vim.defer_fn(function()
+        require("copilot").setup()
+      end, 100)
+    end,
+  }
+  use { "zbirenbaum/copilot-cmp", requires = "copilot.lua", module = "copilot_cmp" }
 
-    use {
-        "zbirenbaum/copilot.lua",
-        event = { "VimEnter" },
-        config = function()
-            vim.defer_fn(function()
-                require("copilot").setup()
-            end, 100)
-        end,
-    }
-    use { "zbirenbaum/copilot-cmp", requires = "copilot.lua", module = "copilot_cmp" }
+  -- Easy installation of LSPs and DAP Servers (the antedecent to lspconfig)
+  use {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup {}
+    end,
+  }
 
-    -- Easy installation of LSPs and DAP Servers (the antedecent to lspconfig)
-    use {
-        "williamboman/mason.nvim",
-        config = function()
-            require "mason".setup {}
-        end,
-    }
+  use {
+    "williamboman/mason-lspconfig.nvim",
+    requires = { "mason.nvim", "nvim-lspconfig" },
+    config = function()
+      require("mason-lspconfig").setup {}
+    end,
+  }
 
-    use {
-        "williamboman/mason-lspconfig.nvim",
-        requires = { "mason.nvim", "nvim-lspconfig" },
-        config = function()
-            require "mason-lspconfig".setup {}
-        end,
-    }
+  -- Used for formatting
+  use {
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+      require "configs.null-ls"
+    end,
+  }
 
-    -- Used for formatting
-    use {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-            require "configs.null-ls"
-        end,
-    }
+  -- UI Stuff
 
-    -- UI Stuff
+  use {
+    "weilbith/nvim-code-action-menu",
+    cmd = "CodeActionMenu",
+  }
+  use {
+    "ray-x/lsp_signature.nvim",
+  }
 
-    use {
-        'weilbith/nvim-code-action-menu',
-        cmd = 'CodeActionMenu',
-    }
-    use {
-        "ray-x/lsp_signature.nvim",
-    }
+  -- Treesitter for better highlighting and improved movement
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    requires = { "windwp/nvim-ts-autotag", "windwp/nvim-autopairs" },
+    config = function()
+      require "configs/treesitter"
+      require "nvim-treesitter.install"
+      require("nvim-autopairs").setup {}
+    end,
+  }
 
-    -- Treesitter for better highlighting and improved movement
-    use {
-        "nvim-treesitter/nvim-treesitter",
-        requires = { 'windwp/nvim-ts-autotag', "windwp/nvim-autopairs" },
-        config = function()
-            require("configs/treesitter")
-            require('nvim-treesitter.install')
-            require("nvim-autopairs").setup {}
-        end,
-    }
+  use {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require "configs/indent-blankline"
+    end,
+  }
 
-    use {
-        'lukas-reineke/indent-blankline.nvim',
-        config = function()
-            require("configs/indent-blankline")
-        end,
-    }
+  -- use {
+  --     "m-demare/hlargs.nvim",
+  --     requires = "nvim-treesitter/nvim-treesitter",
+  --     config = function()
+  --         require("hlargs").setup {}
+  --     end,
+  -- }
 
-    -- use {
-    --     "m-demare/hlargs.nvim",
-    --     requires = "nvim-treesitter/nvim-treesitter",
-    --     config = function()
-    --         require("hlargs").setup {}
-    --     end,
-    -- }
+  use {
+    "ziontee113/syntax-tree-surfer",
+    requires = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require "configs/syntax-tree-surfer"
+    end,
+  }
 
-    use {
-        "ziontee113/syntax-tree-surfer",
-        requires = "nvim-treesitter/nvim-treesitter",
-        config = function()
-            require("configs/syntax-tree-surfer")
-        end,
-    }
+  -- Debugging
+  use "mfussenegger/nvim-dap"
+  use "rcarriga/nvim-dap-ui"
+  use "theHamsta/nvim-dap-virtual-text"
+  -- use {
+  --     "simrat39/rust-tools.nvim",
+  --     config = function ()
+  --         require("rust-tools").setup{}
+  --     end,
+  -- }
 
-    -- Debugging
-    use 'mfussenegger/nvim-dap'
-    use "rcarriga/nvim-dap-ui"
-    use "theHamsta/nvim-dap-virtual-text"
-    -- use {
-    --     "simrat39/rust-tools.nvim",
-    --     config = function ()
-    --         require("rust-tools").setup{}
-    --     end,
-    -- }
+  -- Utilities
+  -- use {
+  --   "romgrk/barbar.nvim",
+  --   requires = { "kyazdani42/nvim-web-devicons" },
+  --   config = function()
+  --     require("bufferline").setup { auto_hide = false }
+  --   end,
+  -- }
+  --
+  use {
+    "feline-nvim/feline.nvim",
+    config = function()
+      require("feline").setup {}
+    end,
+  }
 
-    -- Utilities
-    use {
-        'romgrk/barbar.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons' },
-        config = function() require 'bufferline'.setup { auto_hide = false } end,
-    }
-    --
-    use {
-        "tiagovla/scope.nvim",
-        config = function()
-            require "scope".setup {}
-        end
-    }
+  use {
+    "tiagovla/scope.nvim",
+    config = function()
+      require("scope").setup {}
+    end,
+  }
 
-    use {
-        "kylechui/nvim-surround",
-        config = function()
-            require("nvim-surround").setup {}
-        end,
-    }
+  use {
+    "kylechui/nvim-surround",
+    config = function()
+      require("nvim-surround").setup {}
+    end,
+  }
 
-    use {
-        "brenoprata10/nvim-highlight-colors",
-        config = function() require("nvim-highlight-colors").setup {} end,
-    }
+  use {
+    "brenoprata10/nvim-highlight-colors",
+    config = function()
+      require("nvim-highlight-colors").setup {}
+    end,
+  }
 
-    use {
-        "nvim-neo-tree/neo-tree.nvim",
-        requires = "MunifTanjim/nui.nvim",
-        config = function()
-            require("neo-tree").setup {
-                filesystem = {
-                    hijack_netrw_behaviour = "open_default"
-                },
-            }
-        end
-    }
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    requires = "MunifTanjim/nui.nvim",
+    config = function()
+      require("neo-tree").setup {
+        filesystem = {
+          hijack_netrw_behaviour = "open_default",
+        },
+      }
+    end,
+  }
 
-    use {
-        "terrortylor/nvim-comment",
-        config = function() require 'nvim_comment'.setup {} end,
-    }
+  use {
+    "terrortylor/nvim-comment",
+    config = function()
+      require("nvim_comment").setup {}
+    end,
+  }
 
-    use 'JoosepAlviste/nvim-ts-context-commentstring'
+  use "JoosepAlviste/nvim-ts-context-commentstring"
 
-    use {
-        "danymat/neogen",
-        config = function()
-            require('neogen').setup {}
-        end,
-        requires = "nvim-treesitter/nvim-treesitter",
-    }
+  use {
+    "danymat/neogen",
+    config = function()
+      require("neogen").setup {}
+    end,
+    requires = "nvim-treesitter/nvim-treesitter",
+  }
 
-    use { "ellisonleao/glow.nvim" }
-    use "kdheepak/lazygit.nvim"
+  use { "ellisonleao/glow.nvim" }
+  use "kdheepak/lazygit.nvim"
 
-    use {
-        "folke/trouble.nvim",
-        requires = "kyazdani42/nvim-web-devicons",
-        config = function()
-            require("trouble").setup {}
-        end
-    }
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {}
+    end,
+  }
 
-    use {
-        "folke/todo-comments.nvim",
-        requires = "nvim-lua/plenary.nvim",
-        config = function()
-            require("todo-comments").setup {
-                keywords = {
-                    HELP = { icon = "ﲉ", color = "warning" },
-                    CHANGE = { icon = "", color = "error" }
-                },
-            }
-        end
-    }
+  use {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {
+        keywords = {
+          HELP = { icon = "ﲉ", color = "warning" },
+          CHANGE = { icon = "", color = "error" },
+        },
+      }
+    end,
+  }
 
-    use {
-        "folke/which-key.nvim",
-        config = function()
-            require("which-key").setup {}
-        end,
-    }
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup {}
+    end,
+  }
 
-    use {
-        "folke/twilight.nvim",
-        config = function()
-            require("twilight").setup {}
-        end
-    }
+  use {
+    "folke/twilight.nvim",
+    config = function()
+      require("twilight").setup {}
+    end,
+  }
 
-    use "simrat39/symbols-outline.nvim"
+  use "simrat39/symbols-outline.nvim"
 
-    -- NOTE: Change to FTerm since this thing is weird as fuck
-    use { "akinsho/toggleterm.nvim",
-        tag = 'v2.*',
-        config = function()
-            require("toggleterm").setup {}
-        end }
+  -- NOTE: Change to FTerm since this thing is weird as fuck
+  use {
+    "akinsho/toggleterm.nvim",
+    tag = "v2.*",
+    config = function()
+      require("toggleterm").setup {}
+    end,
+  }
 
-    use {
-        'phaazon/hop.nvim',
-        branch = 'v2', -- optional but strongly recommended
-        config = function()
-            -- you can configure Hop the way you like here; see :h hop-config
-            require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
-            require 'which-key'.register { ['\\'] = { "<CMD>HopWord<CR>", "Quick navigation using Hop" } }
-        end
-    }
+  use {
+    "phaazon/hop.nvim",
+    branch = "v2", -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require("hop").setup { keys = "etovxqpdygfblzhckisuran" }
+      require("which-key").register { ["\\"] = { "<CMD>HopWord<CR>", "Quick navigation using Hop" } }
+    end,
+  }
 
-    use { 'michaelb/sniprun', run = 'bash ./install.sh' }
-    use {
-        "rcarriga/nvim-notify",
-        config = function()
-            require("notify").setup {}
-        end
-    }
+  use { "michaelb/sniprun", run = "bash ./install.sh" }
+  use {
+    "rcarriga/nvim-notify",
+    config = function()
+      require("notify").setup {}
+    end,
+  }
 end)
