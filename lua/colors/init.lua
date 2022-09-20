c51190df5c67ccedce8241ef1e3e99ee643d33f1
Toby Bridle <foreignspaghettibolognese@gmail.com>
2022-09-20 13:16:11 +0100

    gruvbox baby is now transparent

--- lua/colors/init.lua
+++ lua/colors/init.lua
@@ -3,4 +3,8 @@ if not has_opts then
   error "core.custom not found!"
 end
 
+if user_opts.cmd.colorscheme == "gruvbox-baby" then
+  vim.g.gruvbox_baby_transparent_mode = 1
+end
+
 vim.cmd("autocmd VimEnter * colorscheme " .. user_opts.cmd.colorscheme) -- Wait for plugins to load
