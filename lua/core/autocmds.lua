2c73116d8d17460dd6e8f0c82e0b98567ec8d616
Toby Bridle <foreignspaghettibolognese@gmail.com>
2022-11-20 16:41:29 +0000

    feat: removed uneeded autocmd

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -11,12 +11,6 @@ local langs = {
 vim.api.nvim_create_autocmd("InsertEnter", { command = "set norelativenumber", pattern = "*" })
 vim.api.nvim_create_autocmd("InsertLeave", { command = "set relativenumber", pattern = "*" })
 
--- Formatting
-vim.api.nvim_create_autocmd("BufWritePre", {
-  command = "lua vim.lsp.buf.formatting_sync(nil, 1000)",
-  pattern = "*.cpp,*.css,*.go,*.h,*.html,*.js,*.json,*.jsx,*.lua,*.md,*.py,*.rs,*.ts,*.tsx,*.yaml",
-})
-
 vim.api.nvim_create_autocmd("TextYankPost", {
   command = "lua vim.highlight.on_yank({higroup = 'IncSearch', timeout = 50})",
   pattern = "*",
