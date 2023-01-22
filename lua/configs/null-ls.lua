6b5452868df3916fcc6987b8f2fee4755ab4474b
Toby Bridle <foreignspaghettibolognese@gmail.com>
2023-01-22 16:02:37 +0000

    feat: updated null-ls

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -1,27 +1,32 @@
 local ok, null_ls = pcall(require, "null-ls")
 if not ok then
-    return
+  return
 end
 
 local formatting = null_ls.builtins.formatting
-require("null-ls").setup({
-    -- you can reuse a shared lspconfig on_attach callback here
-    -- on_attach = function(client, bufnr)
-    --     if client.supports_method("textDocument/formatting") then
-    --         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    --         vim.api.nvim_create_autocmd("BufWritePre", {
-    --             group = augroup,
-    --             buffer = bufnr,
-    --             callback = function()
-    --                 -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-    --                 vim.lsp.buf.formatting_sync()
-    --             end,
-    --         })
-    --     end
-    -- end,
-    sources = {
-        formatting.prettierd,
-        formatting.stylua,
-        formatting.rustfmt,
-    }
-})
+local linting = null_ls.builtins.diagnostics
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+require("null-ls").setup {
+  -- you can reuse a shared lspconfig on_attach callback here
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          vim.lsp.buf.format { bufnr = bufnr }
+        end,
+      })
+    end
+  end,
+  sources = {
+    formatting.prettierd,
+    formatting.stylua,
+    formatting.rustfmt,
+    formatting.csharpier,
+    formatting.clang_format,
+  },
+}
